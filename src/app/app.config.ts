90d73049d5c019cf3c52ae5da5c48f4b25632754
AbubakerImran <abubakerimran4697@gmail.com>
2025-07-17 17:20:57 +0500

    1

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,22 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZonelessChangeDetection } from '@angular/core';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+
+export const firebaseConfig = {
+  projectId: "angular-app-12112",
+  appId: "1:755919150497:web:1d2ab7f393c337edf8bfe0",
+  storageBucket: "angular-app-12112.firebasestorage.app",
+  apiKey: "AIzaSyAIfddf6DRHUWN-UBZAeY2Lkj5HPczlwRI",
+  authDomain: "angular-app-12112.firebaseapp.com",
+  messagingSenderId: "755919150497" 
+};
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZonelessChangeDetection(),
+    provideFirebaseApp(() => initializeApp(firebaseConfig)),
+    provideAuth(() => getAuth()), provideFirestore(() => getFirestore()),
   ]
 };
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,23 +1,73 @@
 import { Component, signal } from '@angular/core';
+import { Auth, createUserWithEmailAndPassword, deleteUser, signOut } from '@angular/fire/auth';
+import { doc, Firestore, setDoc } from '@angular/fire/firestore';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
+  standalone: true,
+  imports: [FormsModule],
   template: `
     <div class="container">
       <p class="heading">Employee Registeration Form</p><br>
       <div class="form-container">
         <label for="name">Name</label><br>
-        <input type="text" placeholder="Enter your name"/><br>
+        <input [(ngModel)]="name" type="text" placeholder="Enter your name" autocapitalize="words"/><br>
         <label for="number">Phone Number</label><br>
-        <input type="tel" placeholder="Enter your phone number"/><br>
+        <input [(ngModel)]="number" type="tel" placeholder="Enter your phone number"/><br>
         <label for="email">Email</label><br>
-        <input type="email" placeholder="Enter your email"/><br>
+        <input [(ngModel)]="email" type="email" placeholder="Enter your email"/><br>
         <label for="password">Password</label><br>
-        <input type="password" placeholder="Enter your password"/><br>
-        <button>Submit</button>
+        <input [(ngModel)]="password" type="password" placeholder="Enter your password"/><br>
+        <button (click)="register()" type="button">@if (loading()) {<div class="spinner-border" role="status" style="margin-left: 19px; margin-right: 19px; width: 25px; height: 25px"></div>} @else {Submit}</button>
       </div>
     </div>
   `,
   styleUrl: './app.css'
 })
-export class App {}
+export class App {
+
+  loading = signal(false);
+
+  name = '';
+  number =  '';
+  email = '';
+  password = '';
+
+  constructor(private firestore: Firestore, private auth: Auth) {}
+  
+  async register() {
+    const data = {
+      name: this.name,
+      number: this.number,
+      email: this.email,
+    };
+    if (!this.name || !this.number || !this.email || !this.password) {
+      alert('Please enter all details!');
+    } else {
+      const emailPattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9]+\.[a-zA-Z]{2,}$/;
+      if ( !emailPattern.test(this.email) ) {
+        alert("Please enter valid email address.");
+      } else {
+        if ( this.password.length < 8 ) {
+          alert('Password must be 8 character long.');
+        } else {
+          try {
+          this.loading.set(true);
+          await createUserWithEmailAndPassword(this.auth, this.email, this.password);
+          await setDoc(doc(this.firestore, 'employees', this.email), data);
+          await signOut(this.auth);
+          this.loading.set(false);
+          alert('Registered successfully!');
+          } catch (err) {
+            this.loading.set(false);
+            if (this.auth.currentUser) {
+              await deleteUser(this.auth.currentUser);
+            }
+            alert(err);
+          }
+        }
+      }
+    } 
+  }
+}
