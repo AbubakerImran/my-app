eaaebf50ec4e7e03bd61138a5dea086e553659fa
AbubakerImran <abubakerimran4697@gmail.com>
2025-07-18 19:20:22 +0500

    Revert "4"
    This reverts commit 5a4403f9f29fd153a22148fdd7c6804c60894fd1.

--- src/app/app.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Routes } from '@angular/router';
-import { DashboardComponent } from './dashboard.component';
-import { RegisterComponent } from './register.component';
-
-export const routes: Routes = [
-  { path: '', redirectTo: 'register', pathMatch: 'full' },
-  { path: 'register', component: RegisterComponent },
-  { path: 'dashboard', component: DashboardComponent },
-];
